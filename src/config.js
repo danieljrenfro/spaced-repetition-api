4f93fbe2c4ff3aac29785cb0526b44f4b97bd714
danieljrenfro <djrenfro95@gmail.com>
2021-03-05 11:03:02 -0600

    fix DATABASE_URL var

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL
+  DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://danielrenfro@localhost/spaced-repetition',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://danielrenfro@localhost/spaced-repetition-test',
   CLIENT_ORIGIN: 'https://spaced-repetition-client-khaki.vercel.app',
